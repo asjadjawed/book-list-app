d6a17f2d0ffffbd5e9ab8bfbaa0fd65e198801ac
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2020-04-11 12:51:32 +0500

    Providing context

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 export class Navbar extends Component {
+  static contextType = ThemeContext;
+
   render() {
+    const { light, dark, isLightTheme } = this.context;
+    const theme = isLightTheme ? light : dark;
     return (
-      <nav>
+      <nav style={{ background: theme.ui, color: theme.syntax }}>
         <h1>Book List App</h1>
         <ul>
           <li>Home</li>
