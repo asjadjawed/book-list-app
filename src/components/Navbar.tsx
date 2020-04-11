1fd61791f91aeb75696362b74905e69b2d35ef74
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2020-04-11 14:09:38 +0500

    Consuming multiple context

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,34 @@
 import React, { Component } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
+import { AuthContext } from "../contexts/AuthContext";
 
 export class Navbar extends Component {
   render() {
     return (
-      <ThemeContext.Consumer>
-        {(context) => {
-          const { light, dark, isLightTheme } = context;
-          const theme = isLightTheme ? light : dark;
-          return (
-            <nav style={{ background: theme.ui, color: theme.syntax }}>
-              <h1>Book List App</h1>
-              <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-              </ul>
-            </nav>
-          );
-        }}
-      </ThemeContext.Consumer>
+      <AuthContext.Consumer>
+        {(authContext) => (
+          <ThemeContext.Consumer>
+            {(themeContext) => {
+              const { isAuthenticated, toggleAuth } = authContext!;
+              const { light, dark, isLightTheme } = themeContext!;
+              const theme = isLightTheme ? light : dark;
+              return (
+                <nav style={{ background: theme.ui, color: theme.syntax }}>
+                  <h1>Book List App</h1>
+                  <div onClick={toggleAuth}>
+                    {isAuthenticated ? "Logged in" : "Logged out"}
+                  </div>
+                  <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                  </ul>
+                </nav>
+              );
+            }}
+          </ThemeContext.Consumer>
+        )}
+      </AuthContext.Consumer>
     );
   }
 }
